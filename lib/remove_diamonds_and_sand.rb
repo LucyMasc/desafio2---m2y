1a6d9bc0a680416c1ee5d99591bd09ced9fdfe02
Luciana Mascarenhas <lucianamascarenhas011@hotmail.com>
2021-10-22 13:53:53 -0300

    Final touchs.

--- lib/remove_diamonds_and_sand.rb
+++ lib/remove_diamonds_and_sand.rb
@@ -1,4 +1,4 @@
-def remove_diamonds_and_sands(chars_exp)
+def remove_diamonds_and_sand(chars_exp)
   # remove all sand
   chars_exp.gsub!(/\./, '')
   size_after_remove_sand = chars_exp.size
@@ -6,5 +6,6 @@ def remove_diamonds_and_sands(chars_exp)
   # keep looping until there's no more diamonds to extract
   chars_exp.gsub!(/<>/, '') while chars_exp.include?('<>')
 
+  # (size_after_remove_sand - size_after_remove_all_diamonds) / 2
   (size_after_remove_sand - chars_exp.size) / 2
 end
--- spec/remove_diamonds_and_sand_spec.rb
+++ spec/remove_diamonds_and_sand_spec.rb
@@ -1,8 +1,8 @@
-require 'remove_diamonds_and_sands'
+require 'remove_diamonds_and_sand'
 
 describe '#remove_diamonds_and_sands' do
   it 'should return total of diamonds removed' do
-    expect(remove_diamonds_and_sands('<<.<<..>><>><.>.>.<<.>.<.>>>><>><>>')).to eq(11)
+    expect(remove_diamonds_and_sand('<<.<<..>><>><.>.>.<<.>.<.>>>><>><>>')).to eq(11)
   end
 end
 
